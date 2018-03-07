c2239d0f450f104492a2d07e0f3cfc6637f5da25
Luv Iyer <luviyer@gmail.com>
2018-03-07 15:24:18 -0500

    Comments and debt to equity metric added

--- csv_file_manipulations.py
+++ csv_file_manipulations.py
@@ -3,10 +3,25 @@ import os
 import datetime
 
 def last_day_of_month(any_day):
+    '''
+
+    :param any_day: (datetime) any day in a particular month and year
+    :return: (datetime) the last day of that month
+
+    '''
+
     next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
     return next_month - datetime.timedelta(days=next_month.day)
 
 def get_all_months_in_year_range(start_year, end_year):
+    '''
+
+    :param start_year: (int) year that the range should start e.g. 2016
+    :param end_year: (int) ending year (months in this year are not included)
+    :return: (list) every month in between the years in '%Y%m' format e.g. ['201601', ..., '201712']
+
+    '''
+
     month_range = []
     for year in range(start_year, end_year):
         for month in range(1, 13):
@@ -17,7 +32,7 @@ def get_all_months_in_year_range(start_year, end_year):
 def csv_separate_permnos(csv_file, folder_name, make_monthly_data):
     '''
 
-    :param csv_file: (str) filename for csv
+    :param csv_file: (str) filename for csv (datadate column formatted as %Y%m%d
     :param folder_name: (str) folder where all the separated files should be saved
     :param make_monthly_data: (boolean) forward fills quarterly data into monthly data
     :return: None
@@ -31,18 +46,30 @@ def csv_separate_permnos(csv_file, folder_name, make_monthly_data):
     for permno in list_of_permnos:
         permno_df = df[df['LPERMNO'] == permno]
 
+        # Only the year and month are necessary for the index
         truncated_months = [str(elem)[:-2] for elem in permno_df['datadate']]
         permno_df.set_index(pd.Series(truncated_months), inplace=True)
 
         if make_monthly_data:
             permno_df = make_monthly_time_series(permno_df)
 
+        # Save the separated csvs to the folder.
         if not os.path.exists(folder_name):
             os.makedirs(folder_name)
         permno_df.to_csv('{}/{}.csv'.format(folder_name, permno))
 
 
 def make_monthly_time_series(df):
+    '''
+
+    :param df: (pandas dataframe) dataframe with index values as strings, in format '%Y%m'. Frequency of
+                data should be greater than monthly.
+    :return: (pandas dataframe) dataframe with index values as strings, in format '%Y%m'. Data is
+                padded, and frequency of data is monthly.
+
+    '''
+
+    # Convert the starting and ending months into datetimes.
     start_month = last_day_of_month(datetime.datetime.strptime(df.index[0], '%Y%m'))
     end_month = last_day_of_month(datetime.datetime.strptime(df.index[-1], '%Y%m'))
 
--- metrics.py
+++ metrics.py
@@ -2,18 +2,33 @@ import pandas as pd
 
 '''Metrics used for quality portfolios are described below'''
 
-def return_on_equity(filename):
-    '''
-    :param filename:
-    :return:
+def return_on_equity(df):
     '''
 
-    df = pd.read_csv(filename, usecols=['LPERMNO', 'datadate', 'atq', 'ltq', 'mkvaltq', 'cshoq', 'epsx12'])
+    :param df: (pandas dataframe) dataframe with the columns used below
+    :return: (pandas dataframe) dataframe with added roe column
+
+    '''
 
     df['bvps'] = (df['atq']-df['ltq'])/(df['cshoq']*df['mkvaltq'])
     df['roe'] = df['epsx12']/df['bvps']
 
-    return df['roe']
+    return df
+
+
+def debt_to_equity(df):
+    '''
+
+    :param df: (pandas dataframe) dataframe with the columns used below
+    :return: (pandas dataframe) dataframe with added dte column
+
+    '''
+
+    df['bv'] = (df['atq']-df['ltq'])/df['mkvaltq']
+    df['dte'] = df['ltq']/df['bv']
+
+    return df
+
 
 if __name__ == '__main__':
     test = return_on_equity()
